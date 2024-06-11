a012d946de389b674975737269dd6195a8421250
dauletisataev <daulet@voyage.finance>
2024-06-11 15:16:46 +0500

    feat: add worker

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import 'dotenv/config';
-import express from 'express';
-import connectDB from './db';
-import globalRouter from './global-router';
-import { logger } from './logger';
-
+import "dotenv/config";
+import express from "express";
+import connectDB from "./db";
+import globalRouter from "./global-router";
+import { logger } from "./logger";
+import { authMiddleware } from "./middlewares/auth-middleware";
+import { Worker } from "worker_threads";
+import { EventModel } from "./events/models/Event";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -11,12 +13,20 @@ connectDB();
 
 app.use(logger);
 app.use(express.json());
-app.use('/api/v1/',globalRouter);
 
+app.use("/api/v1/", globalRouter);
 
-app.get('/helloworld',(request,response) =>{
-  response.send("Hello World!");
-})
+app.get("/slow", async (request, response) => {
+  const worker = new Worker("./src/workers/counterWorker.ts");
+
+  worker.on("message", (message) => {
+    response.send(message);
+  });
+});
+
+app.get("/fast", (request, response) => {
+  response.send("I'm fast one!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server runs at http://localhost:${PORT}`);
